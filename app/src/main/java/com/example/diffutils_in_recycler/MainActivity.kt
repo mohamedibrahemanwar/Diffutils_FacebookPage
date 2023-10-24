2bb43a18d100d2082109ed779c4b65f40411064a
mohamedibrahemanwar <mohamedibrahemofficail@gmail.com>
2023-10-24 21:18:52 +0300

    init app

--- app/src/main/java/com/example/diffutils_in_recycler/MainActivity.kt
+++ app/src/main/java/com/example/diffutils_in_recycler/MainActivity.kt
@@ -21,13 +21,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun loadData() : MutableList<PostsItem>{
+    private fun loadData() : MutableList<PostsItem>{
         val nameList : MutableList<PostsItem> = mutableListOf()
         nameList.add(PostsItem("Mohamed","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
         nameList.add(PostsItem("Ahmed","Hello",R.drawable.user_icon,R.drawable.ic_launcher_background))
         nameList.add(PostsItem("Ali","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
-        nameList.add(PostsItem("Rady","Hello",R.drawable.user_icon,R.drawable.ic_launcher_background))
-        nameList.add(PostsItem("Esraa","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
+        nameList.add(PostsItem("Ready","Hello",R.drawable.user_icon,R.drawable.ic_launcher_background))
+        nameList.add(PostsItem("Esra","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
         nameList.add(PostsItem("Mohamed","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
         nameList.add(PostsItem("Lolo","Hello",R.drawable.user_icon,R.drawable.ic_launcher_foreground))
 
